31c4e9aa3321d9ce0cc10a71e23c30eb799cb497
triptyy <150001066+triptyy@users.noreply.github.com>
2026-02-16 11:13:57 +0530

    update to add

--- Hello.java
+++ Hello.java
@@ -1,8 +1,8 @@
 public class Hello {
 	public static void main(String as[]) {
-		int a = 200;
+		int a = 10;
 		int b = 10;
 		System.out.println("Hello Java");
-		System.out.println("Total =" + (a / b));
+		System.out.println("Total =" + (a * b));
 	}
 }
